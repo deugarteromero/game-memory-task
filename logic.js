44ab91d38dae0a5f4752307a14c5d1b4f51c7b7f
Leonardo M. De Ugarte R <leonardodeu@outlook.com>
2020-11-02 13:34:37 -0800

    Add Delay to Squares to Appear/Disappear

--- logic.js
+++ logic.js
@@ -5,12 +5,35 @@ let viewSquares = document.querySelectorAll('.viewSquare');
 function random(){
   let numberArray = [];
   for(let x = 0; x < 3; x++){
-    numberArray.push(Math.floor((Math.random() * 9) + 1 ));
+    numberArray.push(Math.floor(Math.random() * 9));
   };
   return numberArray;
 };
 
+function animateSquares(){
+  let numberArray = random();
+  // console.log(numberArray);
+  setTimeout( () => {
+    viewSquares[numberArray[0]].classList.add('active');
+  }, 1000);
+  setTimeout( () => {
+    viewSquares[numberArray[0]].classList.remove('active');
+  }, 1400);
 
+  setTimeout( () => {
+    viewSquares[numberArray[1]].classList.add('active');
+  }, 1600);
+  setTimeout( () => {
+    viewSquares[numberArray[1]].classList.remove('active');
+  }, 2100);
 
+  setTimeout( () => {
+    viewSquares[numberArray[2]].classList.add('active');
+  }, 2300);
+  setTimeout( () => {
+    viewSquares[numberArray[2]].classList.remove('active');
+  }, 2800);
+};
 
-console.log(viewSquares);
+animateSquares();
+// console.log(viewSquares);
