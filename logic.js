e1533994e0ac8f3bfeffb803c168f83cb23661a1
Leonardo M. De Ugarte R <leonardodeu@outlook.com>
2020-11-03 11:48:15 -0800

    Major Code change to series gameplay

--- logic.js
+++ logic.js
@@ -3,6 +3,10 @@ let viewSquares = document.querySelectorAll('.viewSquare');
 let inputSquares = document.querySelectorAll('.inputSquare');
 
 //Manipulation
+function random(){
+  return Math.floor(Math.random() * 9);
+};
+
 let userClickedSquares = [];
 for(let n = 0; n <= 8; n++){
   inputSquares[n].addEventListener('click', () => {
@@ -18,56 +22,79 @@ for(let n = 0; n <= 8; n++){
   });
 };
 
-function random(){
-  let numberArray = [];
-  for(let x = 0; x < 3; x++){
-    numberArray.push(Math.floor(Math.random() * 9));
-  };
-  return numberArray;
-};
-
-let numberArray;
+let numberArray = [];
 function animateSquares(){
-  numberArray = random();
-  // console.log(numberArray);
-  setTimeout( () => {
-    viewSquares[numberArray[0]].classList.add('active');
-  }, 1000);
-  setTimeout( () => {
-    viewSquares[numberArray[0]].classList.remove('active');
-  }, 1400);
+  let nextNumber = random();
+  numberArray.push(nextNumber);
+  console.log(numberArray);
 
-  setTimeout( () => {
-    viewSquares[numberArray[1]].classList.add('active');
-  }, 1600);
-  setTimeout( () => {
-    viewSquares[numberArray[1]].classList.remove('active');
-  }, 2100);
+  if(numberArray.length >= 1){
+    setTimeout( () => {
+      viewSquares[numberArray[0]].classList.add('active');
+    }, 1000);
+    setTimeout( () => {
+      viewSquares[numberArray[0]].classList.remove('active');
+    }, 1400);
+  };
 
-  setTimeout( () => {
-    viewSquares[numberArray[2]].classList.add('active');
-  }, 2300);
-  setTimeout( () => {
-    viewSquares[numberArray[2]].classList.remove('active');
-  }, 2800);
+  if(numberArray.length >= 2){
+    setTimeout( () => {
+      viewSquares[numberArray[1]].classList.add('active');
+    }, 1600);
+    setTimeout( () => {
+      viewSquares[numberArray[1]].classList.remove('active');
+    }, 2100);
+  };
+
+  if(numberArray.length >= 3){
+    setTimeout( () => {
+      viewSquares[numberArray[2]].classList.add('active');
+    }, 2300);
+    setTimeout( () => {
+      viewSquares[numberArray[2]].classList.remove('active');
+    }, 2800);
+  };
+
+  if(numberArray.length >= 4){
+    setTimeout( () => {
+      viewSquares[numberArray[3]].classList.add('active');
+    }, 3000);
+    setTimeout( () => {
+      viewSquares[numberArray[3]].classList.remove('active');
+    }, 3500);
+  };
+
+  if(numberArray.length >= 5){
+    setTimeout( () => {
+      viewSquares[numberArray[4]].classList.add('active');
+    }, 3700);
+    setTimeout( () => {
+      viewSquares[numberArray[4]].classList.remove('active');
+    }, 4200);
+  };
 
 };
 
 function verifyUserInput(numArr, userArr) {
-  if(numArr.length === 3 && userArr.length === 3){
-    for(let i = 0; i < 3; i++){
+  if(numArr.length === userArr.length){
+    for(let i = 0; i < numArr.length; i++){
       if(numArr[i] !== userArr[i]){
         //Make User Friendly Error Message
         return alert("Wrong");
       };
     };
 
-    return inputSquares.forEach(element => {
-      element.classList.add('pass');
-      setTimeout( () => {
-        element.classList.remove('pass');
-      }, 300);
-    });
+    if(numArr.length === 5){
+      return inputSquares.forEach(element => {
+        element.classList.add('pass');
+        setTimeout( () => {
+          element.classList.remove('pass');
+        }, 300);
+      });
+    } else {
+      userClickedSquares = [];
+      animateSquares();
+    };
 
   };
 };
